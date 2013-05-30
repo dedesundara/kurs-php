e889b97b8386ef56e5250cb4f59e8b7ef5b46ca9
Tommy Surbakti <tommy@surbakti.net>
2013-05-30 16:33:57 +0700

    Penambahan Disclaimer :P

--- nisp.php
+++ nisp.php
@@ -1,4 +1,24 @@
-<?php
+<?php 
+/**
+* Kurs Bank Bank di Indonesia
+*
+* @author tommy
+* @copyright 2013 Tommy A. Surbakti tommy@surbakti.net
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+* License as published by the Free Software Foundation; either
+* version 3 of the License, or any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+*
+* You should have received a copy of the GNU Affero General Public
+* License along with this library. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
 function fungsiCurl($url){
      $data = curl_init();
      curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
