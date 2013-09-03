708c5ff47e2de00106e9bb839ac166c9b59d4032
Tommy Surbakti <tommy@surbakti.net>
2013-09-03 09:52:52 +0700

    Bank China Trust

---
+++ chinatrust.php
@@ -0,0 +1,36 @@
+<?php 
+/**
+* Kurs Bank Bank di Indonesia
+*
+* @author tommy
+* @copyright 2013 Tommy A. Surbakti tommy@surbakti.net
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+* License as published by the Free Software Foundation; either
+* version 3 of the License, or any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+*
+* You should have received a copy of the GNU Affero General Public
+* License along with this library. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+// chinatrust.php
+function fungsiCurl($url){
+     $data = curl_init();
+     curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
+     curl_setopt($data, CURLOPT_URL, $url);
+         curl_setopt($data, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6");
+     $hasil = curl_exec($data);
+     curl_close($data);
+     return $hasil;
+}
+$url = fungsiCurl('http://www.chinatrust.co.id/');
+$pecah = explode('<div class="textFooter" style="width:170px; margin-left:auto; margin-right:auto;">:', $url);
+$pecah2 = explode ('Rates are subject to change without prior notice. For Bank Notes, please consult with customer service.',$pecah[1]);
+print_r ($pecah2[0]);
+?>
